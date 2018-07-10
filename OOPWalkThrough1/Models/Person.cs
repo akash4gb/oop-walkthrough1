d77360df4b7cf3b132d9b74fde6c4de5600852d4
Akash Khan <akash4gb@gmail.com>
2018-07-10 13:17:36 +0600

    reverse name working

---
+++ OOPWalkThrough1/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPWalkThrough1.Models
+{
+    class Person
+    {
+        public string firstName;
+        public string middleName;
+        public string lastName;
+        private string fullName;
+   
+
+        public string GetFullName()
+        {
+            fullName = firstName + " " + middleName + " " + lastName;
+            return fullName;
+        }
+
+        public string GetReverseName()
+        {
+            char[] reversename = fullName.ToCharArray();
+            Array.Reverse(reversename);
+            return new string(reversename);
+
+        }
+    }
+  
+}
--- OOPWalkThrough1/PersonInformationUI.cs
+++ OOPWalkThrough1/PersonInformationUI.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using OOPWalkThrough1.Models;
 
 namespace OOPWalkThrough1
 {
@@ -10,9 +11,15 @@ namespace OOPWalkThrough1
             InitializeComponent();
         }
 
+        Person person1 = new Person();
         private void showButton_Click(object sender, EventArgs e)
         {
-           
+            person1.firstName = firstNameTextBox.Text;
+            person1.middleName = middleNameTextBox.Text;
+            person1.lastName = lastNameTextBox.Text;
+
+            fullNameTextBox.Text=person1.GetFullName();
+            reverseNameTextBox.Text = person1.GetReverseName();
         }
     }
 }
